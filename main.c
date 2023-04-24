9186c7b60330bbbdcd063ef9bf900fffb8c73717
Gecazo <filip.zalozyk@gmail.com>
2023-04-24 15:05:59 +0200

    Add get_input function

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <conio.h>
 
 const int SCREEN_WIDTH = 12;
 const int SCREEN_HEIGHT = 20;
@@ -8,4 +10,14 @@ void display_message(const char *message, int yOffset){
     strcpy(buffer, message);
     print_at_xy(SCREEN_WIDTH/2 - strlen(message)/2, 
                 (SCREEN_HEIGHT/2 - 1)+yOffset, buffer);
+}
+
+char get_input(){
+    char ch = 0;
+
+    if(kbhit()){
+        ch = getch();
+    }
+
+    return ch;
 }
